02e0e3f8135ee66f900ddd8bf74da6bbf8b6b183
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-05-13 12:31:21 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Component/LogIn-Out/Login.jsx";
+import Header from './Component/Layout/Header.jsx';
+import Home from './Component/Home.jsx';
 
 export default function App() {
   return (
-    <div>linkedIn clone</div>
-  )
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/home' element={<>
+          <Header />
+          <Home />
+        </>} />
+      </Routes>
+    </Router>
+  );
 }
